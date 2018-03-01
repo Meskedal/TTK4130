b5c470fc33c14305ed2870dd2c9b0fc4677c9642
Meskedal <mariuesk@stud.ntnu.no>
2018-03-01 20:14:55 +0100

    minor cleans

--- Matlab/WW_format.m
+++ Matlab/WW_format.m
@@ -1,7 +1,6 @@
 function [values, variable_name] = WW_format(mat_file, variable)
     mat_file = "BenchPlant - assignment " + mat_file + ".mat";
     load(mat_file);
-    name = name';
     index = WW_find_index(name, variable);
     variable_name = name(index, :);
     variable_name = variable_name;
--- Matlab/WWplots.m
+++ Matlab/WWplots.m
@@ -1,10 +1,9 @@
+%Load mat file, open names and copy desired variable to function
 [t, t_name] = WW_format('2d', 'Time');
 [v, v_name] = WW_format('2d','Settler.Xf');
 [v2, v2_name] = WW_format('2d','Settler.rXi                 ');
-%[v2, v2_name]= WW_format('2d', 'Settler.S1.So');
 
 figure(1);
-%suptitle('1d dry');
 hold on; grid on;
 p1 = plot(t,v); p1.LineWidth = 2;
 p1 = plot(t,v2); p1.LineWidth = 2;
